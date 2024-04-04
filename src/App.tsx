bf58449152dee00675bdfe3860be1dff54fff019
mattiullah <117710678+Mattiullah134@users.noreply.github.com>
2024-04-05 02:59:46 +0500

    change the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <main>
-      <p>Application for test the ci knowledge</p>
+      <p>Application for test the ci knowledge change the file for test the repo</p>
     </main>
   );
 }
